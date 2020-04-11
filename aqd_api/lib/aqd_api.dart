1b46d9f2db3ef7363fe62320cf7368060e41f67f
Gerson Ramos <gr@gersonramos.com>
2020-04-11 17:37:46 -0300

    models aula

---
+++ aqd_api/lib/aqd_api.dart
@@ -0,0 +1,11 @@
+/// aqd_api
+///
+/// A Aqueduct web server.
+library aqd_api;
+
+export 'dart:async';
+export 'dart:io';
+
+export 'package:aqueduct/aqueduct.dart';
+
+export 'channel.dart';
---
+++ aqd_api/lib/model/categoria_model.dart
@@ -0,0 +1,23 @@
+import 'package:aqd_api/aqd_api.dart';
+import 'package:aqd_api/model/movimentacao_model.dart';
+
+class CategoriaModel extends ManagedObject<_CategoriaModel> implements _CategoriaModel {}
+
+enum TipoCategoria{
+  receita, despesa
+}
+
+@Table(name: 'categoria')
+class _CategoriaModel {
+  @Column(primaryKey: true)
+  int id;
+
+  @Column()
+  String nome;
+
+  @Column()
+  TipoCategoria tipoCategoria;
+
+  ManagedSet<MovimentacaoModel> movimentacoes;
+  
+}
